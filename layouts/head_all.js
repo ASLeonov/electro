f1c239fb0712bb6271a4eee3dccfc1bc19a279b3
Alex_L <asleonov@inbox.ru>
2020-04-04 13:45:46 +0300

    11th commit

--- layouts/head_all.js
+++ layouts/head_all.js
@@ -3,9 +3,10 @@ import Head from 'next/head'
 export default function MyHead_all(props) {
   return (
     <Head>
-      <title>{(props.title) ? `Next.js example - ${props.title}` : 'error_title'}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport"       content="initial-scale=1.0, width=device-width" />
+      <title>123</title>
+      {/* <title>{(props.title) ? `Next.js example - ${props.title}` : 'error_title'}</title> */}
+      {/* <meta charSet="utf-8" />
+      <meta name="viewport"       content="initial-scale=1.0, width=device-width" /> */}
       <meta name="Description"    content="Next.js - React framework etc..."/>
       <meta name="Keywords"       content="Next.js, React, SSR, Front-end development"/>
     </Head>
